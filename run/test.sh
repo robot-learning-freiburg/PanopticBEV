419b17210da198072d29006cc0f261b77dc4f192
Sami Laubo <slaubo99@gmail.com>
2023-04-20 08:43:55 +0200

    Create test.sh

---
+++ run/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --workdir /laubo
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 8G
+#SBATCH --partition gpu
+#SBATCH --gres gpu:1
+#SBATCH --qos dlav
+#SBATCH --account civil-459
+#SBATCH --reservation civil-459
+
